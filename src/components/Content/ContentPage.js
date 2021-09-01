af2b3951a621cf1ed6327695e341fc6d7b1f05ef
nghiatic <pnghiaacoustic@gmail.com>
2021-09-01 18:18:08 +0700

    Content 70%

--- src/components/Content/ContentPage.js
+++ src/components/Content/ContentPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { AiFillEye } from 'react-icons/ai'
+import { FiShoppingCart } from 'react-icons/fi'
 import "../css/ContentPage.css"
 export default class ContentPage extends Component {
     render() {
@@ -19,31 +21,231 @@ export default class ContentPage extends Component {
                                 </h3>
                                 <hr />
                             </div>
-                            <div className="products">
-                                <div className="product-item">
-                                    <div className="product-img-wrap">
-                                        <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
-                                        <span className="product-sale">-13%</span>
-                                        <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
-                                        <span className="quick-view"></span>
-                                        <span className="add-cart"></span>
-                                    </div>
-                                    <div className="product-info-wrap">
-                                        <div className="product-name">
-                                            <a>Sweater personality</a>
-                                        </div>
-                                        <div className="product-price">
-                                            <span>
-                                                <del>$90</del>
-                                                <ins>$79</ins>
+                            <Row className="products">
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
                                             </span>
                                         </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
+                                            </span>
+                                        </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
+                                            </span>
+                                        </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
+                                            </span>
+                                        </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
+                                            </span>
+                                        </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={4}>
+                                    <div className="product-item">
+                                        <div className="product-img-wrap">
+                                            <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_6_1-yellow_360x.jpg?v=1538643905" alt="" />
+                                            <span className="product-sale">-13%</span>
+                                            <img className="product-img-hover" src="https://cdn.shopify.com/s/files/1/0025/5082/5007/products/5559315300_2_2_1_360x.jpg?v=1538643905" alt="" />
+                                            <span className="quick-view">
+                                                <AiFillEye />
+                                            </span>
+                                            <span className="add-cart">
+                                                <FiShoppingCart />
+                                            </span>
+                                        </div>
+                                        <div className="product-info-wrap">
+                                            <div className="product-name">
+                                                <a>Sweater personality</a>
+                                            </div>
+                                            <div className="product-price">
+                                                <span className="product-price-content">
+                                                    <del>$90</del>
+                                                    <ins>$79</ins>
+                                                </span>
+                                                <span className="product-color">
+                                                    <span></span>
+                                                    <span></span>
+                                                    <span></span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <div className="bottom-title">
+                                <span className="shop-now">
+                                    <a>Shop now</a>
+                                </span>
+                                <span className="bottom-title-hr"></span>
                             </div>
                         </div>
                     </Col>
                 </Row>
+                <div className="product-adv">
+                    <div className="product-adv-content">
+                        <Row>
+                            <Col sm={6} className="half-adv">
+                                <a>
+                                    <div className="adv-img">
+                                        <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/files/h2-banner1-2_df02bfc0-7eaa-4f7e-a691-7954b807501e_670x.jpg?v=1538736869" alt=""/>
+                                    </div>
+                                    <div className="adv-info">
+                                        <h4>Mini backpack</h4>
+                                        <h6>Bags & Accessories</h6>
+                                    </div>
+                                </a>
+                            </Col>
+                            <Col className="half-adv">
+                                <a>
+                                    <div className="adv-img">
+                                        <img src="https://cdn.shopify.com/s/files/1/0025/5082/5007/files/h2-banner21_34ccd090-afbf-4259-88cc-0a776b5fcf57_670x.jpg?v=1538736918" alt=""/>
+                                    </div>
+                                    <div className="adv-info">
+                                        <h5>Big Sale</h5>
+                                        <h5>50% OFF</h5>
+                                    </div>
+                                </a>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
             </div>
         )
     }
